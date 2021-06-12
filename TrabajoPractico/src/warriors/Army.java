b1becddf002fc1f4df1de20c9cf01cd86fb0451e
Hernán Matías Rubio <matiihr@outlook.es>
2021-06-12 12:03:28 -0300

    Cambios estéticos.
    Nueva interface "Curable". Cambios de nombre a variables.
    Reimplementación de algunos métodos.

--- TrabajoPractico/src/warriors/Army.java
+++ TrabajoPractico/src/warriors/Army.java
@@ -7,21 +7,25 @@ public class Army extends MilitaryUnit {
 	private PriorityQueue<MilitaryUnit> army;
 	
 	public void addMilitaryUnit(MilitaryUnit other) {
+		
 		this.army.add(other);
 	}
 	
 	@Override
 	public void attack(Warrior anotherWarrior) {
-		army.peek().attack(anotherWarrior);
+		
+		this.army.peek().attack(anotherWarrior);
 	}
 
 	@Override
 	public void receiveAttack(double damage) {
+		
 		army.peek().receiveAttack(damage);
 	}
 
 	@Override
 	public void rest() {
+		
 		army.peek().rest();
 	}
 }
